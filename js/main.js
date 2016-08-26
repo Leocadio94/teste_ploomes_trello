c8651fbf5816d9a8f3ded5ebb2b88838756755d2
Gabriel Leocádio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-08-26 20:01:19 -0300

    Aspectos visuais do trello

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
     $(".sortable-board").sortable({
         opacity: 0.7,
         placeholder: "ui-state-highlight",
-        forcePlaceholderSize: true
+        forcePlaceholderSize: true,
+        items: ".list"
     });
     $(".cards").sortable({
         placeholder: "ui-state-highlight",
         forcePlaceholderSize: true,
         connectWith: ".cards"
+    });
+    $.getJSON("lista.json", function(json) {
+        console.log(JSON.stringify(json));
     });
 });
